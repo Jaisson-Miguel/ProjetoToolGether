3897239a9c20ce5afb22700faa3cc55de113c847
Wesley Bruno <engwesleybruno@gmail.com>
2025-05-08 21:08:22 -0300

    teste

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Servidor funcionando!" });
+  return response.json({ message: "Servidor funcionando! 222" });
 });
 
 app.get("/teste", (request, response) => {
